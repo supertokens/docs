9c2e63015576a648e4e710cc108cc76fd3de637e
Rishabh <rishabh@supertokens.io>
2021-07-19 13:55:21 +0530

    adds recipe label in navbar

--- v2/src/theme/NavbarItem/RecipeLabelItem.js
+++ v2/src/theme/NavbarItem/RecipeLabelItem.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { isMobile } from 'react-device-detect';
 
 export default function RecipeLabelItem(props) {
+    if (isMobile) {
+        return null;
+    }
     return (
-        <>TODO:</>
+        <span
+            style={{
+                fontSize: "16px",
+                fontStyle: "italic"
+            }}>{props.label}<span style={{
+                fontStyle: "normal"
+            }}>{" | "}</span><a href="/docs/community/recipes">See All Recipes</a></span>
     );
 }
