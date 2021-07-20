094c097ed3d34f46a9cec8e47d93c83b1b225e64
Joel Coutinho <joelcoutinho@qually.io>
2021-07-20 18:11:51 +0530

    added database tabs, self-hosted-with-docker changes

---
+++ v2/src/components/tabs/DatabaseTabs.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+let Tabs = require("@theme/Tabs").default;
+
+export default function DatabaseTabs(props) {
+    return (
+        <Tabs
+            groupId="database-tabs"
+            defaultValue="mysql"
+            values={[
+                { label: 'MySQL', value: 'mysql' },
+                { label: 'PostgreSQL', value: 'postgresql'},
+                { label: 'MongoDB', value: 'mongodb'}
+            ]}>
+            {props.children}
+        </Tabs>
+    );
+}
