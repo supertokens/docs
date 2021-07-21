8faa3c5ccf0e409d17234832f9c0eb2aeb8a2c36
Rishabh <rishabh@supertokens.io>
2021-07-21 12:31:40 +0530

    small changes

--- v2/src/components/tabs/FrontendSDKTabs.tsx
+++ v2/src/components/tabs/FrontendSDKTabs.tsx
@@ -41,7 +41,7 @@ function DefaultVanillaJSTabItem() {
                 <div className="admonition-content">
                     <ul>
                         <li>We support only the <a href="/docs/session/introduction">session recipe</a> for VanillaJS.</li>
-                        <li>You can use the session recipe SDK and build the login UI yourself by directly integrating with <a href="/docs/thirdpartyemailpassword/apis">our APIs</a></li>
+                        <li>You can use the session recipe and build the login UI yourself by directly integrating with <a href="/docs/thirdpartyemailpassword/apis">our APIs</a></li>
                     </ul>
                 </div>
             </div>
