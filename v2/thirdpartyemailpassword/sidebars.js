741c3db6bfbe15375c4dafc03d38e0b92b3ee3f0
Rishabh <rishabh@supertokens.io>
2021-07-21 15:53:26 +0530

    fixes sign in section

--- v2/thirdpartyemailpassword/sidebars.js
+++ v2/thirdpartyemailpassword/sidebars.js
@@ -52,7 +52,6 @@ module.exports = {
           type: "category",
           label: "Sign In Form",
           items: [
-            "common-customizations/signin-form/about",
             "common-customizations/signin-form/field-validators",
             "common-customizations/signin-form/built-in-providers",
             "common-customizations/signin-form/custom-providers",
