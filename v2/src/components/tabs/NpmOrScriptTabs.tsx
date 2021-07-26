909a560aaef0e004cc5f39ce354c499cc081759d
Rishabh <rishabh@supertokens.io>
2021-07-26 18:46:58 +0530

    docs mod

---
+++ v2/src/components/tabs/NpmOrScriptTabs.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+let Tabs = require("@theme/Tabs").default;
+let TabItem = require("@theme/TabItem").default;
+
+export default function NpmOrScriptTabs(props) {
+    return (
+        <Tabs
+            groupId="npm-or-script"
+            defaultValue="npm"
+            values={[
+                { label: 'Via NPM', value: 'npm' },
+                { label: 'Via Script Tag', value: 'script' },
+            ]}>
+            {props.children}
+        </Tabs>
+    );
+}
