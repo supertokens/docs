baca18a7ac60faab688929cb85598794ef9f8047
Rishabh <rishabh@supertokens.io>
2021-08-16 12:00:30 +0530

    fix to landing page

--- v2/src/pages/index.js
+++ v2/src/pages/index.js
@@ -12,7 +12,7 @@ function Hello() {
                     fontSize: '20px',
                 }}>
                 <p>
-                    <a href="/docs/contribute/introduction">Click here</a> to see the user docs
+                    <a href="/docs/community/introduction">Click here</a> to see the user docs
                 </p>
             </div>
 
