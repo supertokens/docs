8b3230294076073482bbac5ec1c24a99f581d68f
Rishabh <rishabh@supertokens.io>
2021-09-13 15:58:49 +0530

    adds more code snippets

---
+++ v2/src/components/tabs/GoFrameworkSubTabs.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+let Tabs = require("@theme/Tabs").default;
+let TabItem = require("@theme/TabItem").default;
+
+export default function GoFrameworkSubTabs(props) {
+    return (
+        <Tabs
+            isSubTab={true}
+            groupId="go-framework"
+            defaultValue="http"
+            values={[
+                { label: 'net/http', value: 'http' },
+                { label: 'Gin', value: 'gin' },
+                { label: 'Chi', value: 'chi' },
+                { label: 'Mux', value: 'mux' },
+                { label: 'Twirp', value: 'twirp' },
+            ]}>
+            {props.children}
+        </Tabs>
+    );
+}
