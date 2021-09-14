8c116474c73f5c28ddccf40b249c0ea0abf3955a
Mihaly Lengyel <mihaly.lengyel@tresorit.com>
2021-09-14 22:23:37 +0200

    Added NestJS docs to all NodeJSFrameworkSubTabs

--- v2/src/components/tabs/NodeJSFrameworkSubTabsServerless.tsx
+++ v2/src/components/tabs/NodeJSFrameworkSubTabsServerless.tsx
@@ -20,6 +20,7 @@ export default function NodeJSFrameworkSubTabsServerless(props) {
             ]}>
             {childContainsTabItemWithValue("serverless", props.children) ? null : DefaultServerlessTabItem()}
             {childContainsTabItemWithValue("nextjs", props.children) ? null : DefaultNextjsTabItem()}
+            {childContainsTabItemWithValue("nestjs", props.children) ? null : DefaultNestJSTabItem()}
             {props.children}
         </Tabs>
     );
@@ -63,6 +64,28 @@ function DefaultNextjsTabItem() {
                 </div>
                 <div className="admonition-content">
                     <p>Please refer the <strong>NextJS</strong> section (Towards the bottom of the navigation index)</p>
+                </div>
+            </div>
+        </TabItem>
+    );
+}
+
+function DefaultNestJSTabItem() {
+    return (
+        <TabItem value="nestjs">
+            <div className="admonition admonition-info alert alert--info">
+                <div className="admonition-heading">
+                    <h5>
+                        <span className="admonition-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16">
+                                <path fill-rule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path>
+                            </svg>
+                        </span>
+                        info
+                    </h5>
+                </div>
+                <div className="admonition-content">
+                    <p>Please refer the <strong>NestJS</strong> section (Towards the bottom of the navigation index)</p>
                 </div>
             </div>
         </TabItem>
