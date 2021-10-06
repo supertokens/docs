7c48366959dd668647f36b7943594731d6343fc0
Rishabh <rishabh@supertokens.io>
2021-10-06 13:53:16 +0530

    small changes

--- v2/src/components/tabs/FrontendSDKTabs.tsx
+++ v2/src/components/tabs/FrontendSDKTabs.tsx
@@ -11,9 +11,11 @@ export default function FrontendSDKTabs(props) {
             values={[
                 { label: 'ReactJS', value: 'reactjs' },
                 { label: 'Plain JavaScript', value: 'vanillajs' },
+                { label: 'React Native', value: 'react-native' },
             ]}>
             {childContainsTabItemWithValue("reactjs", props.children) ? null : DefaultReactJSTabItem()}
             {childContainsTabItemWithValue("vanillajs", props.children) ? null : DefaultVanillaJSTabItem()}
+            {childContainsTabItemWithValue("react-native", props.children) ? null : DefaultRNTabItem()}
             {props.children}
         </Tabs>
     );
@@ -22,6 +24,30 @@ export default function FrontendSDKTabs(props) {
 function DefaultReactJSTabItem() {
     return <>Should never show this since we support ReactJS SDK for everything...</>;
 }
+
+function DefaultRNTabItem() {
+    return (
+        <TabItem value="react-native">
+            <div className="admonition admonition-important alert alert--info">
+                <div className="admonition-heading">
+                    <h5>
+                        <span className="admonition-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16">
+                                <path fill-rule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z">
+                                </path>
+                            </svg>
+                        </span>
+                        coming soon
+                    </h5>
+                </div>
+                <div className="admonition-content">
+                    <p>In the meantime, you can subscribe to <a href="https://github.com/supertokens/supertokens-react-native">our react native SDK</a> to get notified about releases.</p>
+                </div>
+            </div>
+        </TabItem>
+    );
+}
+
 
 function DefaultVanillaJSTabItem() {
     return (
