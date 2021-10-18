1ebc8dad58e60d62e36fde41fa33b0226398d50e
Rishabh <rishabh@supertokens.io>
2021-10-18 22:38:59 +0530

    removes RN SDK docs

--- v2/docusaurus.config.js
+++ v2/docusaurus.config.js
@@ -90,16 +90,6 @@ module.exports = {
           dropdownItemsBefore: [],
           // Do not add the link active class when browsing docs.
           dropdownActiveClassDisabled: true,
-          docsPluginId: 'react-native',
-        },
-        {
-          type: 'docsVersionDropdown',
-          //// Optional
-          position: 'left',
-          // Add additional dropdown items at the beginning/end of the dropdown.
-          dropdownItemsBefore: [],
-          // Do not add the link active class when browsing docs.
-          dropdownActiveClassDisabled: true,
           docsPluginId: 'python',
         },
         {
@@ -276,18 +266,6 @@ module.exports = {
         path: 'golang',
         routeBasePath: 'docs/golang',
         sidebarPath: require.resolve('./golang/sidebars.js'),
-        showLastUpdateTime: true,
-        editUrl: 'https://github.com/supertokens/docs/tree/master/v2/',
-        remarkPlugins: remarkPlugins,
-      },
-    ],
-    [
-      '@docusaurus/plugin-content-docs',
-      {
-        id: 'react-native',
-        path: 'react-native',
-        routeBasePath: 'docs/react-native',
-        sidebarPath: require.resolve('./react-native/sidebars.js'),
         showLastUpdateTime: true,
         editUrl: 'https://github.com/supertokens/docs/tree/master/v2/',
         remarkPlugins: remarkPlugins,
