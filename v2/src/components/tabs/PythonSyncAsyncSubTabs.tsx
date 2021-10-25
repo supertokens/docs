0690147e17881674412e54be7c301f4b8e8365b7
Bhumil Sarvaiya <sarvaiyabhumil@gmail.com>
2021-10-25 14:16:39 +0530

    python-sdk docs update

--- v2/src/components/tabs/PythonSyncAsyncSubTabs.tsx
+++ v2/src/components/tabs/PythonSyncAsyncSubTabs.tsx
@@ -2,16 +2,15 @@ import React from "react";
 let Tabs = require("@theme/Tabs").default;
 let TabItem = require("@theme/TabItem").default;
 
-export default function PythonFrameworkSubTabs(props) {
+export default function PythonSyncAsyncSubTabs(props) {
     return (
         <Tabs
             isSubTab={true}
             groupId="python-framework"
             defaultValue="fastapi"
             values={[
-                { label: 'FastAPI', value: 'fastapi' },
-                { label: 'Flask', value: 'flask' },
-                { label: 'Django', value: 'django' }
+                { label: 'Asyncio', value: 'asyncio' },
+                { label: 'Syncio', value: 'syncio' }
             ]}>
             {props.children}
         </Tabs>
