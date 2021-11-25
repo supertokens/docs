a471c009bc28e3ddb511cad942f1c15f88dba9e7
Rishabh <rishabh@supertokens.io>
2021-11-25 15:54:35 +0530

    adds docs for sending custom response by throwing an error

--- v2/thirdparty/sidebars.js
+++ v2/thirdparty/sidebars.js
@@ -201,7 +201,14 @@ module.exports = {
           items: [
             "advanced-customizations/apis-override/about",
             "advanced-customizations/apis-override/usage",
-            "advanced-customizations/apis-override/custom-response",
+            {
+              type: 'category',
+              label: 'Sending custom response',
+              items: [
+                "advanced-customizations/apis-override/custom-response/api-override",
+                "advanced-customizations/apis-override/custom-response/throwing-error"
+              ],
+            },
             "advanced-customizations/apis-override/disabling"
           ],
         },
