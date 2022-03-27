f08a13999e12887863e3e8fc341ef49d2730aacf
Jay Mistry <jay@supertokens.io>
2022-03-27 17:37:47 +0530

    updated error message for netlify functions apibasepath prefix error

--- v2/src/components/appInfoForm/index.tsx
+++ v2/src/components/appInfoForm/index.tsx
@@ -677,7 +677,7 @@ export default class AppInfoForm extends React.PureComponent<PropsWithChildren<P
                     ) {
                         // if the netlify api route checkbox is set to true
                         // the api base path can only start with `/.netlify/functions`
-                        validationErrors.apiBasePath = "apiBasePath should begin with '/.netlify/functions/' when using Netlify Serverless Functions."
+                        validationErrors.apiBasePath = "apiBasePath must be prefixed with `/.netlify/functions`, for example to use `/auth` set the apiBasePath to `/.netlify/functions/auth`";
                     } else if (netlifyApiRouteUsed && validApiBasePath === "/.netlify/functions/") {
                         // if the netlify api route is `/.netlify/functions/` without any scope
                         // we show this error, as functions at `/.netlify/functions/` route aren't possible
