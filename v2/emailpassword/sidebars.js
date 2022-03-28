bbb13b4c572efa3c0181bc89b2f63718433c135d
Mihaly Lengyel <mihaly.lengyel@tresorit.com>
2022-03-28 10:21:56 +0200

    Implementing review feedback

--- v2/emailpassword/sidebars.js
+++ v2/emailpassword/sidebars.js
@@ -149,18 +149,6 @@ module.exports = {
             "common-customizations/sessions/fetch-sessions-for-user",
             "common-customizations/sessions/update-jwt-payload",
             "common-customizations/sessions/update-session-data",
-            {
-              type: 'category',
-              label: 'User Metadata',
-              items: [
-                "common-customizations/usermetadata/about",
-                "common-customizations/usermetadata/setup",
-                "common-customizations/usermetadata/store-data",
-                "common-customizations/usermetadata/get-data",
-                "common-customizations/usermetadata/clear-data",
-                "common-customizations/usermetadata/overrides",
-              ],
-            },
             "common-customizations/sessions/multiple-api-endpoints",
             "common-customizations/sessions/cookies-and-https",
             "common-customizations/sessions/cookie-consent",
@@ -171,6 +159,18 @@ module.exports = {
             "common-customizations/sessions/in-iframe",
             "common-customizations/sessions/error-handling"
           ]
+        },
+        {
+          type: 'category',
+          label: 'User Metadata',
+          items: [
+            "common-customizations/usermetadata/about",
+            "common-customizations/usermetadata/setup",
+            "common-customizations/usermetadata/store-data",
+            "common-customizations/usermetadata/get-data",
+            "common-customizations/usermetadata/clear-data",
+            "common-customizations/usermetadata/overrides",
+          ],
         },
         {
           type: "category",
--- v2/thirdparty/sidebars.js
+++ v2/thirdparty/sidebars.js
@@ -133,18 +133,6 @@ module.exports = {
             "common-customizations/sessions/fetch-sessions-for-user",
             "common-customizations/sessions/update-jwt-payload",
             "common-customizations/sessions/update-session-data",
-            {
-              type: 'category',
-              label: 'User Metadata',
-              items: [
-                "common-customizations/usermetadata/about",
-                "common-customizations/usermetadata/setup",
-                "common-customizations/usermetadata/store-data",
-                "common-customizations/usermetadata/get-data",
-                "common-customizations/usermetadata/clear-data",
-                "common-customizations/usermetadata/overrides",
-              ],
-            },
             "common-customizations/sessions/multiple-api-endpoints",
             "common-customizations/sessions/cookies-and-https",
             "common-customizations/sessions/cookie-consent",
@@ -155,6 +143,18 @@ module.exports = {
             "common-customizations/sessions/in-iframe",
             "common-customizations/sessions/error-handling"
           ]
+        },
+        {
+          type: 'category',
+          label: 'User Metadata',
+          items: [
+            "common-customizations/usermetadata/about",
+            "common-customizations/usermetadata/setup",
+            "common-customizations/usermetadata/store-data",
+            "common-customizations/usermetadata/get-data",
+            "common-customizations/usermetadata/clear-data",
+            "common-customizations/usermetadata/overrides",
+          ],
         },
         {
           type: "category",
