3ff93cec3031c21b0ef24f7054543e9e0147aedb
Jay Mistry <jay@supertokens.com>
2022-05-23 10:47:22 +0530

    format name of signed in user before displaying in navbar

--- v2/src/theme/NavbarItem/SignUpButton/index.tsx
+++ v2/src/theme/NavbarItem/SignUpButton/index.tsx
@@ -33,7 +33,37 @@ export default function SignUpButton() {
 async function fetchUserName(): Promise<string | undefined> {
     try {
         const { name } = await getUserInformation();
-        return name;
+        const formattedName = formatNameToDisplayInNavbar(name);
+        return formattedName;
     } catch (e) { }
     return undefined;
+}
+
+/**
+ * Use this method to format the full name of the user to display in the navbar
+ * @param fullName full name of the user fetched from the API
+ * @returns formatted name to display in the navbar
+ */
+ export function formatNameToDisplayInNavbar(fullName: string): string {
+    let userName = "";
+
+    if (fullName !== undefined) {
+        const [firstName, middleName, ..._] = fullName.split(" ");
+
+        if (firstName.length > 8) {
+            // if the first name is longer than 8 characters
+            // display the first 8 characters and append '...'
+            userName = `${firstName.substring(0, 8)}...`;
+        } else if (firstName.length <= 8 && middleName !== undefined) {
+            // if the first name is shorter than 9 characters
+            // and there is a string defined in `middleName`
+            // display the first name and initial from `middleName`
+            const initial = middleName[0].toUpperCase();
+            userName = `${firstName} ${initial}.`;
+        } else {
+            userName = firstName;
+        }
+    }
+
+    return userName;
 }
