c472e472be56f65a3496b6bec175a7eec8d43e07
jscyo <joelsteve97@hotmail.com>
2022-06-08 16:02:30 +0530

    adds angular snippet docs

--- v2/src/components/tabs/FrontendSDKTabs.tsx
+++ v2/src/components/tabs/FrontendSDKTabs.tsx
@@ -12,10 +12,12 @@ export default function FrontendSDKTabs(props: any) {
                 { label: 'ReactJS', value: 'reactjs' },
                 { label: 'Plain JavaScript', value: 'vanillajs' },
                 { label: 'React Native', value: 'react-native' },
+                { label: 'Angular', value: 'angular' },
             ]}>
             {childContainsTabItemWithValue("reactjs", props.children) ? null : DefaultReactJSTabItem()}
             {childContainsTabItemWithValue("vanillajs", props.children) ? null : DefaultVanillaJSTabItem()}
             {childContainsTabItemWithValue("react-native", props.children) ? null : DefaultRNTabItem()}
+            {childContainsTabItemWithValue("angular", props.children) ? null : DefaultAngularTabItem()}
             {props.children}
         </Tabs>
     );
@@ -72,6 +74,35 @@ function DefaultVanillaJSTabItem() {
                     To add login functionality, you need to build your own UI and call the APIs exposed by the backend SDKs. You can find the API spec <a href="https://supertokens.com/docs/fdi">here</a><br/><br/>
 
                     You can refer to <a href="https://supertokens.com/blog/adding-social-login-to-your-website-with-supertokens">this blog post</a> to know how this is done, the example uses social login but the same setup applies to other recipes as well.
+                </div>
+            </div>
+        </TabItem>
+    );
+}
+
+function DefaultAngularTabItem() {
+    return (
+        <TabItem value="angular">
+            <div className="admonition admonition-caution alert alert--warning">
+                <div className="admonition-heading">
+                    <h5>
+                        <span className="admonition-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M8.893 1.5c-.183-.31-.52-.5-.887-.5s-.703.19-.886.5L.138 13.499a.98.98 0 0 0 0 1.001c.193.31.53.501.886.501h13.964c.367 0 .704-.19.877-.5a1.03 1.03 0 0 0 .01-1.002L8.893 1.5zm.133 11.497H6.987v-2.003h2.039v2.003zm0-3.004H6.987V5.987h2.039v4.006z">
+                                </path>
+                            </svg>
+                        </span>
+                        Note
+                    </h5>
+                </div>
+                <div className="admonition-content">
+                    If you are using the pre-built UI you can refer to the <b>ReactJS</b> tab since you will be using the <code>supertokens-auth-react</code> SDK.<br/><br/>
+
+
+                    If you are using custom UI you can refer to the <b>Plain JavaScript</b> section<br/><br/>
+
+                    
+                    We also have an <a href="https://github.com/supertokens/supertokens-auth-react/tree/master/examples/with-angular-thirdpartyemailpassword">example app</a> , the example app uses pre-built UI with email-password and social login but the same setup applies to other recipes as well.
                 </div>
             </div>
         </TabItem>
