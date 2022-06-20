40177a98d56d48360e764495ed937ad71b7af304
jscyo <joelsteve97@hotmail.com>
2022-06-20 15:01:52 +0530

    fixs

--- v2/src/components/tabs/FrontendSDKTabs.tsx
+++ v2/src/components/tabs/FrontendSDKTabs.tsx
@@ -2,11 +2,13 @@ import React from "react";
 let Tabs = require("@theme/Tabs").default;
 let TabItem = require("@theme/TabItem").default;
 import { childContainsTabItemWithValue } from "./utils";
-const COPY_TABS = "~COPY-TABS=";
-let tabsUsingCopyDocs: string[] = [];
+import { recursiveMap } from "../utils";
+const copyTabIdentifier = "~COPY-TABS=";
+const tabValues = ["reactjs", "vanillajs", "react-native", "angular"]
+
 
 export default function FrontendSDKTabs(props: any) {
-  tabsUsingCopyDocs = [];
+  let tabsUsingCopyTabs: string[] = []
   return (
     <Tabs
       groupId="frontendsdk"
@@ -25,145 +27,216 @@ export default function FrontendSDKTabs(props: any) {
         ? null
         : DefaultVanillaJSTabItem()}
       {childContainsTabItemWithValue("react-native", props.children)
-        ? null
+        ? containsCopyTabs("react-native", props.children, tabsUsingCopyTabs)
+          ? applyCopyTabs("react-native", props.children)
+          : null
         : DefaultRNTabItem()}
-      {childContainsCopyTabs("angular", props.children, DefaultAngularTabItem)}
-      {printWithoutCopyTabs(props.children)}
+        {childContainsTabItemWithValue("angular", props.children)
+        ? containsCopyTabs("angular", props.children, tabsUsingCopyTabs)
+          ? applyCopyTabs("angular", props.children)
+          : null
+        : DefaultAngularTabItem()}
+      
+      {returnTabsWithoutCopyTabs(props.children, tabsUsingCopyTabs)}
     </Tabs>
   );
 }
 
+function returnTabsWithoutCopyTabs(children: any, tabsUsingCopyTabs: string[]){
+
+  return recursiveMap(children, (child: any) => {
+    return child
+  }, (child: any) => {
+      if(tabsUsingCopyTabs.includes(child.props.value)){
+        return false
+      }
+    return true
+  } )
+
+}
+
+function applyCopyTabs(tabId: string, children: any): any {
+
+  let isTabContent = false;
+  return recursiveMap(
+    children,
+    (child: any) => {
+      if (child.startsWith(copyTabIdentifier)) {
+        let tabToCopyIdentifier = child.split(copyTabIdentifier)[1];
+        let isCopyTabContent = false
+        let contentToInsert = recursiveMap(children, (child: any) => {
+          return child
+        }, (child) => {
+          if(child.props.mdxType === "TabItem" && tabValues.includes(child.props.value)){
+            isCopyTabContent = false
+          }
+          
+          if(child.props.value !== tabToCopyIdentifier && !isCopyTabContent){
+            return false
+          }
+
+          isCopyTabContent = true
+          return true
+        })
+        
+        return contentToInsert.props.children
+      }
+      return child;
+    },
+    (child) => {
+
+      if(child.props.mdxType === "TabItem" && tabValues.includes(child.props.value)){
+        isTabContent = false
+      }
+
+      if (child.props.value !== tabId && !isTabContent) {
+        return false;
+      }
+
+      isTabContent = true;
+
+      return true;
+    }
+  );
+
+}
+
+
+function containsCopyTabs(tabId: string, children: any, tabsUsingCopyTabs: string[]): boolean {
+  let acceptedChildren: any[] = [];
+  recursiveMap(
+    children,
+    (child: any) => {
+      return child;
+    },
+    (child) => {
+      let accepted = false;
+      for (let i = 0; i < acceptedChildren.length; i++) {
+        let currAcceptedChild = acceptedChildren[i];
+        React.Children.forEach(currAcceptedChild, (child2: any) => {
+          accepted = accepted || child2 == child;
+        });
+      }
+      if (accepted === false && child.props.value !== tabId) {
+        return false;
+      }
+      acceptedChildren.push(child);
+      return true;
+    }
+  );
+
+  let doesChildContainCopyTabs = false;
+  for (let i = 0; i < acceptedChildren.length; i++) {
+    let currAcceptedChild = acceptedChildren[i];
+    recursiveMap(
+      currAcceptedChild,
+      (child: any) => {
+        doesChildContainCopyTabs = child.includes(copyTabIdentifier);
+      },
+      () => {
+        return !doesChildContainCopyTabs;
+      }
+    );
+  }
+  if(doesChildContainCopyTabs){
+    tabsUsingCopyTabs.push(tabId)
+  }
+  return doesChildContainCopyTabs;
+}
+
 function DefaultReactJSTabItem() {
-    return <>Should never show this since we support ReactJS SDK for everything...</>;
+  return (
+    <>Should never show this since we support ReactJS SDK for everything...</>
+  );
 }
 
 function DefaultRNTabItem() {
-    return (
-        <TabItem value="react-native">
-            <div className="admonition admonition-caution alert alert--warning">
-                <div className="admonition-heading">
-                    <h5>
-                        <span className="admonition-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M8.893 1.5c-.183-.31-.52-.5-.887-.5s-.703.19-.886.5L.138 13.499a.98.98 0 0 0 0 1.001c.193.31.53.501.886.501h13.964c.367 0 .704-.19.877-.5a1.03 1.03 0 0 0 .01-1.002L8.893 1.5zm.133 11.497H6.987v-2.003h2.039v2.003zm0-3.004H6.987V5.987h2.039v4.006z">
-                                </path>
-                            </svg>
-                        </span>
-                        Note
-                    </h5>
-                </div>
-                <div className="admonition-content">
-                    To use SuperTokens with React Native you need to use the <code>supertokens-react-native</code> SDK. The SDK provides session management features.<br/><br/>
-                    
-                    To add login functionality, you need to build your own UI and call the APIs exposed by the backend SDKs. You can find the API spec <a href="https://supertokens.com/docs/fdi">here</a>
-                </div>
-            </div>
-        </TabItem>
-    );
+  return (
+    <TabItem value="react-native">
+      <div className="admonition admonition-caution alert alert--warning">
+        <div className="admonition-heading">
+          <h5>
+            <span className="admonition-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8.893 1.5c-.183-.31-.52-.5-.887-.5s-.703.19-.886.5L.138 13.499a.98.98 0 0 0 0 1.001c.193.31.53.501.886.501h13.964c.367 0 .704-.19.877-.5a1.03 1.03 0 0 0 .01-1.002L8.893 1.5zm.133 11.497H6.987v-2.003h2.039v2.003zm0-3.004H6.987V5.987h2.039v4.006z"
+                ></path>
+              </svg>
+            </span>
+            Note
+          </h5>
+        </div>
+        <div className="admonition-content">
+          To use SuperTokens with React Native you need to use the{" "}
+          <code>supertokens-react-native</code> SDK. The SDK provides session
+          management features.
+          <br />
+          <br />
+          To add login functionality, you need to build your own UI and call the
+          APIs exposed by the backend SDKs. You can find the API spec{" "}
+          <a href="https://supertokens.com/docs/fdi">here</a>
+        </div>
+      </div>
+    </TabItem>
+  );
 }
 
 function DefaultVanillaJSTabItem() {
-    return (
-        <TabItem value="vanillajs">
-            <div className="admonition admonition-caution alert alert--warning">
-                <div className="admonition-heading">
-                    <h5>
-                        <span className="admonition-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M8.893 1.5c-.183-.31-.52-.5-.887-.5s-.703.19-.886.5L.138 13.499a.98.98 0 0 0 0 1.001c.193.31.53.501.886.501h13.964c.367 0 .704-.19.877-.5a1.03 1.03 0 0 0 .01-1.002L8.893 1.5zm.133 11.497H6.987v-2.003h2.039v2.003zm0-3.004H6.987V5.987h2.039v4.006z">
-                                </path>
-                            </svg>
-                        </span>
-                        Note
-                    </h5>
-                </div>
-                <div className="admonition-content">
-                    To use SuperTokens with plain javascript you need to use the <code>supertokens-website</code> SDK. The SDK provides session management features.<br/><br/>
-
-                    To add login functionality, you need to build your own UI and call the APIs exposed by the backend SDKs. You can find the API spec <a href="https://supertokens.com/docs/fdi">here</a><br/><br/>
-
-                    You can refer to <a href="https://supertokens.com/blog/adding-social-login-to-your-website-with-supertokens">this blog post</a> to know how this is done, the example uses social login but the same setup applies to other recipes as well.
-                </div>
-            </div>
-        </TabItem>
-    );
+  return (
+    <TabItem value="vanillajs">
+      <div className="admonition admonition-caution alert alert--warning">
+        <div className="admonition-heading">
+          <h5>
+            <span className="admonition-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8.893 1.5c-.183-.31-.52-.5-.887-.5s-.703.19-.886.5L.138 13.499a.98.98 0 0 0 0 1.001c.193.31.53.501.886.501h13.964c.367 0 .704-.19.877-.5a1.03 1.03 0 0 0 .01-1.002L8.893 1.5zm.133 11.497H6.987v-2.003h2.039v2.003zm0-3.004H6.987V5.987h2.039v4.006z"
+                ></path>
+              </svg>
+            </span>
+            Note
+          </h5>
+        </div>
+        <div className="admonition-content">
+          To use SuperTokens with plain javascript you need to use the{" "}
+          <code>supertokens-website</code> SDK. The SDK provides session
+          management features.
+          <br />
+          <br />
+          To add login functionality, you need to build your own UI and call the
+          APIs exposed by the backend SDKs. You can find the API spec{" "}
+          <a href="https://supertokens.com/docs/fdi">here</a>
+          <br />
+          <br />
+          You can refer to{" "}
+          <a href="https://supertokens.com/blog/adding-social-login-to-your-website-with-supertokens">
+            this blog post
+          </a>{" "}
+          to know how this is done, the example uses social login but the same
+          setup applies to other recipes as well.
+        </div>
+      </div>
+    </TabItem>
+  );
 }
+
 
 function DefaultAngularTabItem() {
-  return <TabItem value="angular">Default component</TabItem>;
-}
-
-function childContainsCopyTabs(
-  value: string,
-  children: any,
-  defaultOutput: any
-) {
-  for (let child in children) {
-    if (children[child] === undefined || children[child] === null) {
-      continue;
-    }
-    if (children[child].value === value) {
-      return copyTabs(value, children[child].children, children);
-    }
-    if (children[child].props === undefined) {
-      continue;
-    }
-    if (children[child].props.value === value) {
-      return copyTabs(value, children[child].props.children, children);
-    }
-  }
-  return defaultOutput();
-}
-
-function copyTabs(
-  copyTabId: string,
-  copyTabComponent: any,
-  allComponents: any
-) {
-  let tabToCopyId;
-  let finalCopyTabComponent: any[] = [];
-
-  if (!Array.isArray(copyTabComponent)) {
-    
-      if(typeof copyTabComponent.props.children === "string"){
-        tabToCopyId = copyTabComponent.props.children.split(COPY_TABS)[1];
-      } else {
-        finalCopyTabComponent.push(copyTabComponent)
-      }
-    
-  } else {
-    for (let child in copyTabComponent) {
-      if (
-        typeof copyTabComponent[child].props.children === "string" &&
-        copyTabComponent[child].props.children.includes(COPY_TABS)
-      ) {
-        tabToCopyId =
-          copyTabComponent[child].props.children.split(COPY_TABS)[1];
-      } else {
-        finalCopyTabComponent.push(copyTabComponent[child]);
-      }
-    }
-  }
-
-  for (let child in allComponents) {
-    if (allComponents[child].props.value === tabToCopyId) {
-      tabsUsingCopyDocs.push(copyTabId);
-      return (
-        <TabItem value={copyTabId}>
-          {finalCopyTabComponent}
-          {allComponents[child].props.children}
-        </TabItem>
-      );
-    }
-  }
-}
-
-function printWithoutCopyTabs(children: any) {
-  let response = [];
-  for (let child in children) {
-    if (!tabsUsingCopyDocs.includes(children[child].props.value)) {
-      response.push(children[child]);
-    }
-  }
-  return response;
+  return (
+    <TabItem value="angular">
+      Default Component
+    </TabItem>
+  );
 }
