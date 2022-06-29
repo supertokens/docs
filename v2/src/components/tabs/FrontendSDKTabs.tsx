962fcc9fbdfa56cfd4ec4c4a3152f9a7e6e8d3f2
jscyo <joelsteve97@hotmail.com>
2022-06-29 11:28:28 +0530

    fixes

--- v2/src/components/tabs/FrontendSDKTabs.tsx
+++ v2/src/components/tabs/FrontendSDKTabs.tsx
@@ -82,7 +82,7 @@ function DefaultAngularTabItem() {
                       </h5>
                   </div>
                   <div className="admonition-content">
-                      SuperTokens does not provide prebuilt UI for Angular. Instead we will be using the <code>supertokens-auth-react</code> SDK to render login UI and handle authentication flows.
+                      SuperTokens does not provide prebuilt UI for Angular. Instead we will be using the <code>supertokens-auth-react</code> SDK to render login UI.
                   </div>
               </div>
                 <div>
