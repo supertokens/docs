419dd51fe5207b148ae0092f992377dda0a8639b
Nemi Shah <nemishah1212@gmail.com>
2022-07-09 18:06:41 +0530

    Basic setup for user roles

--- v2/userroles/sidebars.js
+++ v2/userroles/sidebars.js
@@ -1,5 +1,21 @@
 module.exports = {
   sidebar: [
-    "introduction"
+    "introduction",
+    {
+      type: "category",
+      label: "Implementation Guide",
+      collapsed: false,
+      items: [
+        "initialisation",
+        "creating-role",
+        "adding-roles-to-user",
+        "adding-roles-to-session",
+        "reading-role-from-session",
+        "reading-role-on-frontend",
+        "managing-roles-and-permissions",
+        "get-all-roles",
+        "delete-roles",
+      ],
+    }
   ]
 };
