6eb9b7a66a41e925f2d9b3aa243f416da4e796fa
Nemi Shah <nemishah1212@gmail.com>
2022-07-26 11:42:21 +0530

    Rename quick setup to initialization

--- v2/thirdpartyemailpassword/sidebars.js
+++ v2/thirdpartyemailpassword/sidebars.js
@@ -4,7 +4,7 @@ module.exports = {
     "architecture",
     {
       type: 'category',
-      label: 'Quick setup',
+      label: 'Initialization',
       collapsed: false,
       items: [
         "quick-setup/video-tutorial",
