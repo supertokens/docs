e1e575fb632b9d5ab5da4fe4c0eefa7e3351998a
shulha yahya <sulha199@gmail.com>
2022-09-01 18:19:25 +0700

    fix: avoid run isMobile on SSG

--- v2/src/theme/NavbarItem/RecipeLabelItem.js
+++ v2/src/theme/NavbarItem/RecipeLabelItem.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { isMobile } from 'react-device-detect';
+import { BrowserView, isMobile } from 'react-device-detect';
 import GuidesLink from "../../components/guidesLink";
 
 export default function RecipeLabelItem(props) {
-    if (isMobile) {
-        return null;
-    }
-    return (
-        <span
-            style={{
-                fontSize: "16px",
-                fontStyle: "italic"
-            }}>{props.label}<span style={{
-                fontStyle: "normal"
-            }}>{" | "}</span><GuidesLink>See All Recipes</GuidesLink></span>
-    );
+    return <BrowserView>{
+        !isMobile && <span
+        style={{
+            fontSize: "16px",
+            fontStyle: "italic"
+        }}>{props.label}<span style={{
+            fontStyle: "normal"
+        }}>{" | "}</span><GuidesLink>See All Recipes</GuidesLink></span>
+    }</BrowserView>
 }
