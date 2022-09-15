5fa581ea59b3eeb9a7eaa84b4a97603382cae82d
rishabhpoddar <rishabh.poddar@gmail.com>
2022-09-15 16:46:41 +0530

    starts work on reset password further reading page

--- v2/thirdpartyemailpassword/sidebars.js
+++ v2/thirdpartyemailpassword/sidebars.js
@@ -46,7 +46,8 @@ module.exports = {
           label: 'Further Reading',
           items: [
             "pre-built-ui/further-reading/email-password-login",
-            "pre-built-ui/further-reading/thirdparty-login"
+            "pre-built-ui/further-reading/thirdparty-login",
+            "pre-built-ui/further-reading/password-reset"
           ]
         },
       ],
