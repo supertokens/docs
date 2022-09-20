f20c729eb65607fe960025856514af933cccc742
rishabhpoddar <rishabh.poddar@gmail.com>
2022-09-20 19:56:02 +0530

    fixes a few issues

--- v2/src/components/tabs/FrontendMobileSubTabs.tsx
+++ v2/src/components/tabs/FrontendMobileSubTabs.tsx
@@ -15,7 +15,22 @@ export default function FrontendMobileSubTabs(props: any) {
                 { label: 'Android', value: 'android' },
                 { label: 'iOS', value: 'ios' }
             ]}>
+            {childContainsTabItemWithValue("ios", props.children)
+                ? null
+                : DefaultiOSTabItem()}
             {props.children}
         </Tabs>
+    );
+}
+
+function DefaultiOSTabItem() {
+    // we add a div cause COPY-TABS won't work
+    // as a direct child of TabItem.
+    return (
+        <TabItem value="ios" mdxType="TabItem">
+            <div>
+                ~COPY-TABS=android
+            </div>
+        </TabItem>
     );
 }
