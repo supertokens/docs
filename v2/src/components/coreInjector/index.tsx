1ecfaaef3c37c3548f203fb0472f509d14215677
Joel Coutinho <joelsteve97@hotmail.com>
2022-09-26 09:18:44 +0000

    implements feedback

--- v2/src/components/coreInjector/index.tsx
+++ v2/src/components/coreInjector/index.tsx
@@ -40,6 +40,7 @@ export default class CoreInjector extends React.PureComponent<PropsWithChildren<
                     c = c.split("^{coreInjector_api_key}").join('""')
                     c = c.split("^{coreInjector_api_key_commented}").join('')
                     c = c.split("^{coreInjector_api_key_commented_with_hash}").join('')
+                    c = c.split("^{coreInjector_uri_without_quotes}").join('')
                 }
                 return c;
             });
@@ -54,6 +55,7 @@ export default class CoreInjector extends React.PureComponent<PropsWithChildren<
                     c = c.split("^{coreInjector_api_key}").join(`"${key}"`)
                     c = c.split("^{coreInjector_api_key_commented}").join('')
                     c = c.split("^{coreInjector_api_key_commented_with_hash}").join('')
+                    c = c.split("^{coreInjector_uri_without_quotes}").join(`${uri}`)
                 }
                 return c;
             });
@@ -66,6 +68,7 @@ export default class CoreInjector extends React.PureComponent<PropsWithChildren<
                 c = c.split("^{coreInjector_api_key}").join('"IF YOU HAVE AN API KEY FOR THE CORE, ADD IT HERE"')
                 c = c.split("^{coreInjector_api_key_commented}").join('// ')
                 c = c.split("^{coreInjector_api_key_commented_with_hash}").join('# ')
+                c = c.split("^{coreInjector_uri_without_quotes}").join(`https://try.supertokens.com`)
             }
             return c;
         });
