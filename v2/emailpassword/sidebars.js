65a5bc3e3425f03ff7d6ca55afcaca14649ed6d0
rishabhpoddar <rishabh.poddar@gmail.com>
2022-09-28 16:07:34 +0530

    adds email password section to ep recipe as well

--- v2/emailpassword/sidebars.js
+++ v2/emailpassword/sidebars.js
@@ -90,6 +90,7 @@ module.exports = {
             },
           ]
         },
+        "custom-ui/email-password-login",
         "custom-ui/securing-routes",
         "custom-ui/sign-out",
         "custom-ui/enable-email-verification",
