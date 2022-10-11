0fb1832e7510ab9a85984602c455bc87d3affd34
rishabhpoddar <rishabh.poddar@gmail.com>
2022-10-11 11:10:08 +0530

    adds more links

--- v2/emailpassword/sidebars.js
+++ v2/emailpassword/sidebars.js
@@ -337,7 +337,8 @@ module.exports = {
             "common-customizations/reset-password/password-reset-email",
             "common-customizations/reset-password/embed-in-page",
             "common-customizations/reset-password/post-reset-password",
-            "common-customizations/reset-password/changing-token-lifetime"
+            "common-customizations/reset-password/changing-token-lifetime",
+            "common-customizations/reset-password/generate-link-manually"
 
           ]
         },
