e049c24b676d721c24f40a04b75a41eb69ecaebb
rishabhpoddar <rishabh.poddar@gmail.com>
2022-10-29 19:00:14 +0530

    changes to modifying JWT claims docs

--- v2/thirdparty/sidebars.js
+++ v2/thirdparty/sidebars.js
@@ -376,7 +376,6 @@ module.exports = {
                 "common-customizations/sessions/with-jwt/custom-claims",
                 "common-customizations/sessions/with-jwt/read-jwt",
                 "common-customizations/sessions/with-jwt/read-claims",
-                "common-customizations/sessions/with-jwt/update-jwt",
                 "common-customizations/sessions/with-jwt/get-jwks-and-issuer",
                 "common-customizations/sessions/with-jwt/get-public-key",
                 "common-customizations/sessions/with-jwt/jwt-verification"
