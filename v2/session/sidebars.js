049717223aadba4b481b2d22867e31b2b61a1ef7
Mihaly Lengyel <mihaly@lengyel.tech>
2023-01-16 09:30:13 +0100

    Remove get-access-token page

--- v2/session/sidebars.js
+++ v2/session/sidebars.js
@@ -183,7 +183,6 @@ module.exports = {
           ]
         },
         "common-customizations/sessions/protecting-frontend-routes",
-        "common-customizations/sessions/get-access-token",
         "common-customizations/sessions/ssr",
         {
           type: "category",
--- v2/thirdpartypasswordless/sidebars.js
+++ v2/thirdpartypasswordless/sidebars.js
@@ -294,7 +294,6 @@ module.exports = {
           ]
         },
         "common-customizations/sessions/protecting-frontend-routes",
-        "common-customizations/sessions/get-access-token",
         "common-customizations/sessions/ssr",
         {
           type: "category",
