e462b838a4ebaea1b7ead3c9962d2c4fc6c2260a
rishabhpoddar <rishabh.poddar@gmail.com>
2023-03-21 18:49:32 +0530

    adds tpep login docs

--- v2/thirdpartyemailpassword/sidebars.js
+++ v2/thirdpartyemailpassword/sidebars.js
@@ -55,6 +55,7 @@ module.exports = {
             "pre-built-ui/sign-out",
             "pre-built-ui/auth-redirection",
             "pre-built-ui/enable-email-verification",
+            "pre-built-ui/multitenant-login",
             {
               type: 'category',
               label: 'Further Reading',
@@ -115,6 +116,7 @@ module.exports = {
             "custom-ui/securing-routes",
             "custom-ui/sign-out",
             "custom-ui/enable-email-verification",
+            "custom-ui/multitenant-login",
           ],
         },
       ],
@@ -389,6 +391,45 @@ module.exports = {
                 "common-customizations/signin-form/field-validators",
               ]
             },
+            {
+              type: "category",
+              label: "Multi tenancy and Enterprise SSO",
+              items: [
+                "common-customizations/multi-tenancy/overview",
+                "common-customizations/multi-tenancy/new-tenant-config",
+                {
+                  type: "category",
+                  label: "Common UX flows",
+                  items: [
+                    "common-customizations/multi-tenancy/common-domain-login",
+                    "common-customizations/multi-tenancy/sub-domain-login"
+                  ]
+                },
+                {
+                  type: 'category',
+                  label: 'SAML Login',
+                  items: [
+                    "common-customizations/saml/what-is-saml",
+                    "common-customizations/saml/saml-login",
+                    {
+                      type: 'category',
+                      label: 'With BoxyHQ',
+                      customProps: {
+                        logoUrl: '/img/logos/boxyhq.png'
+                      },
+                      items: [
+                        "common-customizations/saml/with-boxyhq/what-is-boxyhq",
+                        "common-customizations/saml/with-boxyhq/flow-diagram",
+                        "common-customizations/saml/with-boxyhq/integration-steps",
+                      ],
+                    },
+                  ],
+                },
+                "common-customizations/multi-tenancy/custom-provider",
+                "common-customizations/multi-tenancy/multi-app",
+              ]
+            },
+            "common-customizations/sign-in-and-up/provider-config",
             "common-customizations/signup-form/toc-privacypolicy",
             "common-customizations/embed-sign-in-up-form",
             "common-customizations/password-managers",
@@ -604,26 +645,6 @@ module.exports = {
             "user-roles/managing-roles-and-permissions",
             "user-roles/get-all-roles",
             "user-roles/delete-roles",
-          ],
-        },
-        {
-          type: 'category',
-          label: 'SAML',
-          items: [
-            "common-customizations/saml/what-is-saml",
-            "common-customizations/saml/saml-login",
-            {
-              type: 'category',
-              label: 'With BoxyHQ',
-              customProps: {
-                logoUrl: '/img/logos/boxyhq.png'
-              },
-              items: [
-                "common-customizations/saml/with-boxyhq/what-is-boxyhq",
-                "common-customizations/saml/with-boxyhq/flow-diagram",
-                "common-customizations/saml/with-boxyhq/integration-steps"
-              ],
-            },
           ],
         },
       ]
