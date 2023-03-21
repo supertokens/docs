14aa14e49a3baa58cd1492983a8be037dd90105c
Iresh Sharma <32684272+iresharma@users.noreply.github.com>
2023-03-21 15:18:43 +0530

    recipe name fix

--- v2/src/theme/NavbarItem/recipeSelector.js
+++ v2/src/theme/NavbarItem/recipeSelector.js
@@ -17,7 +17,7 @@ export default function RecipeSelector(props) {
       case "thirdpartyemailpassword":
         return "ThirdPartyEmailPassword Recipe";
       case "phonepassword":
-        return "EmailPassword + Passwordless Recipe";
+        return "Phone Password Recipe";
       case "thirdpartypasswordless":
         return "ThirdPartyPasswordless Recipe";
       case "emailpassword":
