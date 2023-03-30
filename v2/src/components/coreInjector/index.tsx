4a50c73970c127e48d5184f6e61908211349d7cb
rishabhpoddar <rishabh.poddar@gmail.com>
2023-03-30 19:12:39 +0530

    initial attempt

--- v2/src/components/coreInjector/index.tsx
+++ v2/src/components/coreInjector/index.tsx
@@ -3,9 +3,12 @@ import supertokens from "supertokens-website";
 import { recursiveMap } from "../utils";
 import { getSaasApp } from "../api/saas/apps";
 import { MOCK_ENABLED } from "../constants";
+let Tabs = require("@theme/Tabs").default;
+let TabItem = require("@theme/TabItem").default;
 
 type Props = {
     defaultValue?: string
+    showTenantId?: boolean
 };
 
 type State = {
@@ -27,8 +30,33 @@ export default class CoreInjector extends React.PureComponent<PropsWithChildren<
     }
 
     render() {
+        let singleTenant;
+        let multiTenant;
+        let tenantString = "/appid-<APP_ID>/<TENANT_ID>";
+        if (this.props.showTenantId === false) {
+            tenantString = "/appid-<APP_ID>"
+        }
         if (this.state.sessionState === "UNKNOWN") {
-            return recursiveMap(this.props.children, (c: any) => {
+            singleTenant = recursiveMap(this.props.children, (c: any) => {
+                if (typeof c === "string") {
+                    while (c.includes(" ^{coreInjector_connection_uri_comment}")) {
+                        c = c.split(" ^{coreInjector_connection_uri_comment}").join('^{coreInjector_connection_uri_comment}')
+                    }
+                    c = c.split("\n^{coreInjector_connection_uri_comment}").join('')
+                    while (c.includes(" ^{coreInjector_connection_uri_comment_with_hash}")) {
+                        c = c.split(" ^{coreInjector_connection_uri_comment_with_hash}").join('^{coreInjector_connection_uri_comment_with_hash}')
+                    }
+                    c = c.split("\n^{coreInjector_connection_uri_comment_with_hash}").join('')
+                    c = c.split("^{coreInjector_uri}").join('"",');
+                    c = c.split("^{coreInjector_api_key}").join('""')
+                    c = c.split("^{coreInjector_api_key_without_quotes}").join('')
+                    c = c.split("^{coreInjector_api_key_commented}").join('')
+                    c = c.split("^{coreInjector_api_key_commented_with_hash}").join('')
+                    c = c.split("^{coreInjector_uri_without_quotes}").join('')
+                }
+                return c;
+            });
+            multiTenant = recursiveMap(this.props.children, (c: any) => {
                 if (typeof c === "string") {
                     while (c.includes(" ^{coreInjector_connection_uri_comment}")) {
                         c = c.split(" ^{coreInjector_connection_uri_comment}").join('^{coreInjector_connection_uri_comment}')
@@ -50,7 +78,7 @@ export default class CoreInjector extends React.PureComponent<PropsWithChildren<
         } else if (this.state.sessionState === "EXISTS") {
             let uri = this.state.uri;
             let key = this.state.key;
-            return recursiveMap(this.props.children, (c: any) => {
+            singleTenant = recursiveMap(this.props.children, (c: any) => {
                 if (typeof c === "string") {
                     c = c.split("^{coreInjector_connection_uri_comment}").join('// These are the connection details of the app you created on supertokens.com')
                     c = c.split("^{coreInjector_connection_uri_comment_with_hash}").join('# These are the connection details of the app you created on supertokens.com')
@@ -63,21 +91,65 @@ export default class CoreInjector extends React.PureComponent<PropsWithChildren<
                 }
                 return c;
             });
+            multiTenant = recursiveMap(this.props.children, (c: any) => {
+                if (typeof c === "string") {
+                    c = c.split("^{coreInjector_connection_uri_comment}").join('// These are the connection details of the app you created on supertokens.com')
+                    c = c.split("^{coreInjector_connection_uri_comment_with_hash}").join('# These are the connection details of the app you created on supertokens.com')
+                    c = c.split("^{coreInjector_uri}").join(`"${uri}${tenantString}",`);
+                    c = c.split("^{coreInjector_api_key}").join(`"${key}"`)
+                    c = c.split("^{coreInjector_api_key_without_quotes}").join(`${key}`)
+                    c = c.split("^{coreInjector_api_key_commented}").join('')
+                    c = c.split("^{coreInjector_api_key_commented_with_hash}").join('')
+                    c = c.split("^{coreInjector_uri_without_quotes}").join(`${uri}${tenantString}`)
+                }
+                return c;
+            });
+        } else {
+            singleTenant = recursiveMap(this.props.children, (c: any) => {
+                let defaultValue = this.props.defaultValue || "https://try.supertokens.com";
+                if (typeof c === "string") {
+                    c = c.split("^{coreInjector_connection_uri_comment}").join('// ' + defaultValue + ' is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.')
+                    c = c.split("^{coreInjector_connection_uri_comment_with_hash}").join('# ' + defaultValue + ' is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.')
+                    c = c.split("^{coreInjector_uri}").join('"' + defaultValue + '",');
+                    c = c.split("^{coreInjector_api_key}").join('<API_KEY(if configured)>')
+                    c = c.split("^{coreInjector_api_key_without_quotes}").join('<API_KEY(if configured)>')
+                    c = c.split("^{coreInjector_api_key_commented}").join('// ')
+                    c = c.split("^{coreInjector_api_key_commented_with_hash}").join('# ')
+                    c = c.split("^{coreInjector_uri_without_quotes}").join(defaultValue)
+                }
+                return c;
+            });
+            multiTenant = recursiveMap(this.props.children, (c: any) => {
+                let defaultValue = this.props.defaultValue || "https://try.supertokens.com";
+                if (typeof c === "string") {
+                    c = c.split("^{coreInjector_connection_uri_comment}").join('// ' + defaultValue + ' is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.')
+                    c = c.split("^{coreInjector_connection_uri_comment_with_hash}").join('# ' + defaultValue + ' is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.')
+                    c = c.split("^{coreInjector_uri}").join('"' + defaultValue + tenantString + '",');
+                    c = c.split("^{coreInjector_api_key}").join('<API_KEY(if configured)>')
+                    c = c.split("^{coreInjector_api_key_without_quotes}").join('<API_KEY(if configured)>')
+                    c = c.split("^{coreInjector_api_key_commented}").join('// ')
+                    c = c.split("^{coreInjector_api_key_commented_with_hash}").join('# ')
+                    c = c.split("^{coreInjector_uri_without_quotes}").join(defaultValue + tenantString)
+                }
+                return c;
+            });
         }
-        return recursiveMap(this.props.children, (c: any) => {
-            let defaultValue = this.props.defaultValue || "https://try.supertokens.com";
-            if (typeof c === "string") {
-                c = c.split("^{coreInjector_connection_uri_comment}").join('// ' + defaultValue + ' is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.')
-                c = c.split("^{coreInjector_connection_uri_comment_with_hash}").join('# ' + defaultValue + ' is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.')
-                c = c.split("^{coreInjector_uri}").join('"' + defaultValue + '",');
-                c = c.split("^{coreInjector_api_key}").join('<API_KEY(if configured)>')
-                c = c.split("^{coreInjector_api_key_without_quotes}").join('<API_KEY(if configured)>')
-                c = c.split("^{coreInjector_api_key_commented}").join('// ')
-                c = c.split("^{coreInjector_api_key_commented_with_hash}").join('# ')
-                c = c.split("^{coreInjector_uri_without_quotes}").join(defaultValue)
-            }
-            return c;
-        });
+
+        return <Tabs
+            isSubTab={true}
+            groupId="curl-single-tenant"
+            defaultValue={"single-tenant"}
+            values={[
+                { label: 'Single tenant setup', value: 'single-tenant' },
+                { label: 'Multi tenant / app setup', value: 'multi-tenant' },
+            ]}>
+            <TabItem value="single-tenant" mdxType="TabItem">
+                {singleTenant}
+            </TabItem>
+            <TabItem value="multi-tenant" mdxType="TabItem">
+                {multiTenant}
+            </TabItem>
+        </Tabs>
     }
 
     async componentDidMount() {
