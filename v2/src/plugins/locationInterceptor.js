97885dde0260b8df5bb442842ffe1c4230e525a6
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2023-10-20 15:21:29 +0530

    feat: add  events

--- v2/src/plugins/locationInterceptor.js
+++ v2/src/plugins/locationInterceptor.js
@@ -1,15 +1,16 @@
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
 export default (function() {
-
-    if(!ExecutionEnvironment.canUseDOM){
+    if (!ExecutionEnvironment.canUseDOM) {
         return null;
     }
 
     return {
         onRouteUpdate() {
             if (window.stAnalytics) {
-                window.stAnalytics.getInstance().sendPageViewEvents();
+                const analytics = window.stAnalytics.getInstance();
+                analytics.sendPageViewEvents();
+                analytics.sendPageViewEventToSegment();
             }
         }
     };
