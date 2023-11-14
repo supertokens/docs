25dc23148fa3c37b023afc9c834db429d4af01f2
amitbadala <amitbadala07@gmail.com>
2023-11-14 15:45:41 +0530

    Propogating new features from EmailPassword to thirdparty with COPY DOCS

--- v2/thirdpartyemailpassword/sidebars.js
+++ v2/thirdpartyemailpassword/sidebars.js
@@ -403,7 +403,7 @@ module.exports = {
               label: 'Email password sign up form',
               items: [
                 "common-customizations/signup-form/adding-fields",
-                "common-customizations/signup-form/changing-field-labels",
+                "common-customizations/signup-form/additional-field-props",
                 "common-customizations/signup-form/field-validators",
               ],
             },
@@ -411,7 +411,7 @@ module.exports = {
               type: "category",
               label: "Email password sign in form",
               items: [
-                "common-customizations/signin-form/changing-field-labels",
+                "common-customizations/signin-form/additional-field-props",
                 "common-customizations/signin-form/field-validators",
               ]
             },
