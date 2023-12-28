aa8487540e499c829caf2bab8accd58b9efdc1e1
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2023-12-28 18:39:04 +0530

    Added version select component

--- v2/src/components/compatibility/index.tsx
+++ v2/src/components/compatibility/index.tsx
@@ -15,12 +15,47 @@ export default function CompatibilityMatrix() {
     const [supportedFrontendSdks, setSupportedFrontendSdks] = useState<SdkType[] | undefined>(undefined);
     const [supportedBackendSdks, setSupportedBackedSdks] = useState<SdkType[] | undefined>(undefined);
 
+    const [selectedFrontendSdk, setSelectedFrontendSdk] = useState<SdkType | undefined>({
+        id: "auth-react",
+        displayName: "auth-react"
+    });
+    const [selectedBackendSdk, setSelectedBackendnSdk] = useState<SdkType | undefined>({
+        id: "node",
+        displayName: "node"
+    });
+    const [selectedCoreVersion, setSelectedCoreVersion] = useState<SdkType | undefined>(undefined);
+    const [selectedBackendSdkVersion, setSelectedBackendSdkVersion] = useState<SdkType | undefined>(undefined);
 
-    const [selectedFrontendSdk, setSelectedFrontendSdk] = useState<string | undefined>(undefined);
-    const [selectedBackendSdk, setSelectedBackendnSdk] = useState<string | undefined>(undefined);
+    const [compatibilityMatrix, setCompatibilityMatrix] = useState<GetCompatibilityResponse | undefined>(undefined);
 
-    const [compatibilityMatrix, setCompatibilityMatrix] = useState<GetCompatibilityResponse | undefined>(undefined)
+    let selectableCoreVersions: SdkType[] = [];
+    let selectableBackendSdkVersions: SdkType[] = [];
 
+    if (compatibilityMatrix !== undefined) {
+        selectableCoreVersions = compatibilityMatrix.cores.map(version => {
+            return {
+                id: version,
+                displayName: version
+            };
+        });
+
+        if (selectedCoreVersion !== undefined) {
+            selectableBackendSdkVersions = compatibilityMatrix.coreToDriver[selectedCoreVersion.id].map(version => {
+                return {
+                    id: version,
+                    displayName: version
+                };
+            });
+        } else {
+            const latestCoreVersion = Object.keys(compatibilityMatrix.coreToDriver)[0];
+            selectableBackendSdkVersions = compatibilityMatrix.coreToDriver[latestCoreVersion].map(version => {
+                return {
+                    id: version,
+                    displayName: version
+                };
+            });
+        }
+    }
 
     useEffect(() => {
         async function getSupportedSdks() {
@@ -40,17 +75,17 @@ export default function CompatibilityMatrix() {
         async function getCompatibilityMatrix() {
             if (selectedBackendSdk !== undefined && selectedFrontendSdk !== undefined) {
                 try {
-                    const response = await getCompatibility(selectedBackendSdk, selectedFrontendSdk)
-                    setCompatibilityMatrix(response)
+                    const response = await getCompatibility(selectedBackendSdk.id, selectedFrontendSdk.id);
+                    setCompatibilityMatrix(response);
                 } catch (error) {
-                    alert("Something went wrong, Please try again!")
+                    console.log({ error });
+                    alert("Something went wrong, Please try again!");
                 }
             }
         }
-        getCompatibilityMatrix()
-    }, [selectedBackendSdk, selectedFrontendSdk])
+        getCompatibilityMatrix();
+    }, [selectedBackendSdk, selectedFrontendSdk]);
 
-    console.log({ compatibilityMatrix })
     return (
         <section>
             <div className="compatibility_matrix_box">
@@ -61,50 +96,133 @@ export default function CompatibilityMatrix() {
                     <div className="sdk-selection-container">
                         <div>
                             <span className="compatibility_matrix_text_one">Backend SDK</span>
-                            <select name="backend" id="backend-sdk" onChange={(e) => setSelectedBackendnSdk(e.currentTarget.value)}>
-                                {supportedBackendSdks?.map(sdk => {
-                                    return <option value={sdk.id}>{sdk.displayName}</option>
-                                })}
-                            </select>
+                            {supportedBackendSdks !== undefined ? (
+                                <Select
+                                    onOptionSelect={setSelectedBackendnSdk}
+                                    options={supportedBackendSdks}
+                                    selectedOption={selectedBackendSdk}
+                                />
+                            ) : (
+                                <div className="shimmer" style={{ height: "30px", minWidth: "140px" }}></div>
+                            )}
                         </div>
                         <div>
-                            <span className="compatibility_matrix_text_one" >Frontend SDK</span>
-                            <select name="frontend" id="frotend-sdk" onChange={(e) => setSelectedFrontendSdk(e.currentTarget.value)}>
-                                {supportedFrontendSdks?.map(sdk => {
-                                    return <option value={sdk.id}>{sdk.displayName}</option>
-                                })}
-                            </select>
+                            <span className="compatibility_matrix_text_one">Frontend SDK</span>
+                            {supportedFrontendSdks !== undefined ? (
+                                <Select
+                                    onOptionSelect={setSelectedFrontendSdk}
+                                    options={supportedFrontendSdks}
+                                    selectedOption={selectedFrontendSdk}
+                                />
+                            ) : (
+                                <div className="shimmer" style={{ height: "30px", minWidth: "140px" }}></div>
+                            )}
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="sdk-selection-container-bottom">
-                <div className="compatibility_matrix_box">
-                    <div className="compatibility_matrix_box_header">
-                        <span className="compatibility_matrix_title_two">supertokens-core</span>
-                    </div>
-                    <div className="compatibility_matrix_box_body  border-radius-0">
-                        <select name="backend" id="backend-sdk" onChange={(e) => setSelectedBackendnSdk(e.currentTarget.value)}>
-                            {supportedBackendSdks?.map(sdk => {
-                                return <option value={sdk.id}>{sdk.displayName}</option>
+            {selectedBackendSdk !== undefined && selectedFrontendSdk !== undefined ? (
+                <div className="sdk-selection-container-bottom">
+                    <div className="compatibility_matrix_box w-half">
+                        <div className="compatibility_matrix_box_header">
+                            <span className="compatibility_matrix_title_two">supertokens-core</span>
+                        </div>
+                        <div className="compatibility_matrix_box_body  border-radius-0">
+                            {compatibilityMatrix !== undefined ? (
+                                <Select
+                                    onOptionSelect={setSelectedCoreVersion}
+                                    options={selectableCoreVersions}
+                                    selectedOption={selectedCoreVersion}
+                                />
+                            ) : (
+                                <div className="shimmer" style={{ height: "30px", minWidth: "140px" }}></div>
+                            )}
+                        </div>
+                        <div className="version-pills-container">
+                            {selectableBackendSdkVersions.map(({ id }) => {
+                                return <span className="version-pill">{id}</span>;
                             })}
-                        </select>
+                        </div>
                     </div>
-
+                    <div className="compatibility_matrix_box w-half">
+                        <div className="compatibility_matrix_box_header">
+                            <span className="compatibility_matrix_title_two">{selectedBackendSdk.displayName}</span>
+                        </div>
+                        <div className="compatibility_matrix_box_body border-radius-0">
+                            {compatibilityMatrix !== undefined ? (
+                                <Select
+                                    onOptionSelect={setSelectedBackendSdkVersion}
+                                    options={selectableBackendSdkVersions}
+                                    selectedOption={selectedBackendSdkVersion}
+                                />
+                            ) : (
+                                <div className="shimmer" style={{ height: "30px", minWidth: "140px" }}></div>
+                            )}
+                        </div>
+                    </div>
                 </div>
-                <div className="compatibility_matrix_box">
-                    <div className="compatibility_matrix_box_header">
-                        <span className="compatibility_matrix_title_two">supertokens-node</span>
-                    </div>
-                    <div className="compatibility_matrix_box_body border-radius-0">
-                        <select name="backend" id="backend-sdk" onChange={(e) => setSelectedBackendnSdk(e.currentTarget.value)}>
-                            {supportedBackendSdks?.map(sdk => {
-                                return <option value={sdk.id}>{sdk.displayName}</option>
-                            })}
-                        </select>
-                    </div>
-                </div>
-            </div>
+            ) : null}
         </section>
+    );
+}
+
+type SelectProps = {
+    options: SdkType[];
+    selectedOption: SdkType | undefined;
+    onOptionSelect: (value: SdkType) => void;
+};
+
+function Select({ onOptionSelect, options, selectedOption }: SelectProps) {
+    const [showOptions, setShowOptions] = useState(false);
+    return (
+        <div
+            className="select-container"
+            onMouseLeave={() => setShowOptions(false)}
+            onMouseEnter={() => setShowOptions(true)}
+            //	this code make sure that the select options show up in the mobile view
+            //	since there will be no onMouseEnter event there.
+            onClick={() => setShowOptions(true)}
+        >
+            <div className="select-action">
+                {selectedOption !== undefined ? selectedOption.id : "Please select"}{" "}
+                <svg
+                    style={{ rotate: showOptions ? "180deg" : undefined }}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <path
+                        d="M14 8L10 12L6 8"
+                        stroke="#AAAAAA"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    />
+                </svg>
+            </div>
+            {showOptions ? (
+                <div className="select-dropdown-wrapper">
+                    <div className="select-dropdown">
+                        {options.map(option => {
+                            return (
+                                <div
+                                    className="select-item"
+                                    key={option.id}
+                                    onClick={e => {
+                                        e.stopPropagation();
+                                        onOptionSelect(option);
+                                        setShowOptions(false);
+                                    }}
+                                >
+                                    {option.id}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            ) : null}
+        </div>
     );
 }
