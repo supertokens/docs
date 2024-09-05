13747859904331c2f8d9457b878b5420abc3f991
Victor Bojica <victor@niftylearning.io>
2024-09-05 16:33:25 +0300

    pr fixes

--- v2/anomalydetection/sidebars.js
+++ v2/anomalydetection/sidebars.js
@@ -1,11 +1,3 @@
 module.exports = {
-  sidebar: [
-    "introduction",
-    {
-      type: "category",
-      label: "Setup Guide",
-      collapsed: false,
-      items: ["frontend-setup", "backend-setup"],
-    },
-  ],
+  sidebar: ["introduction", "frontend-setup", "backend-setup"],
 };
