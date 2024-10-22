83f001b2b8fbb5cc4264f30cb5f79ffec05c9768
Bogdan Carpusor <bogdan@supertokens.com>
2024-10-22 16:13:38 +0100

    Update the custom ui guide

--- v2/src/components/OAuthDisclaimer.tsx
+++ v2/src/components/OAuthDisclaimer.tsx
@@ -9,7 +9,7 @@ function OAuthVerifyTokensDisclaimer() {
       <div className="admonition-content">
         <p>
           This guide only applies to scenarios in which you are using{" "}
-          <strong>SuperTokens Access Tokens</strong>.
+          <strong>SuperTokens Session Access Tokens</strong>.
         </p>
         <p>
           If you are implementing{" "}
@@ -27,4 +27,32 @@ function OAuthVerifyTokensDisclaimer() {
   );
 }
 
-export { OAuthVerifyTokensDisclaimer };
+function OAuthEmailVerificationDisclaimer() {
+  return (
+    <div className="admonition admonition-caution alert alert--warning">
+      <div className="admonition-heading">
+        <h5>CAUTION</h5>
+      </div>
+      <div className="admonition-content">
+        <p>
+          This information only applies to scenarios in which you are using{" "}
+          <strong>SuperTokens Session Access Tokens</strong>.
+        </p>
+        <p>
+          If you are implementing{" "}
+          <a href="/docs/unified-login/introduction">
+            <strong>Unified Login</strong>
+          </a>{" "}
+          you will have to manually check the <code>email_verified</code> claim
+          on the <strong>OAuth2 Access Tokens</strong>. Please read the{" "}
+          <a href="/docs/unified-login/customizations/verify-tokens">
+            separate page
+          </a>{" "}
+          that shows you how to verify the token.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export { OAuthVerifyTokensDisclaimer, OAuthEmailVerificationDisclaimer };
