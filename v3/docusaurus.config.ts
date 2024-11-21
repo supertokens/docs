b82f1a16f413f52795e97a83794ecb0c9c0fc115
Bogdan Carpusor <bogdan@supertokens.com>
2024-11-21 20:52:50 +0200

    Trigger build

--- v3/docusaurus.config.ts
+++ v3/docusaurus.config.ts
@@ -13,7 +13,6 @@ const config: Config = {
   trailingSlash: false,
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
-
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
