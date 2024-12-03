2d0f07dc8d7d991ceacd7f58564c479a73b61f74
Deepjyoti Barman <deepjyoti@supertokens.com>
2024-12-03 11:30:05 +0530

    Fix compilation issue

--- v2/src/components/reusableSnippets/angularUIImplementation.tsx
+++ v2/src/components/reusableSnippets/angularUIImplementation.tsx
@@ -5,7 +5,6 @@ export default function AngularUIImplementation(props: PropsWithChildren<{}>) {
   return (
     <Question
       question="What type of UI are you using?"
-      persistentId="angular-ui"
     >
     {props.children}
     </Question>
