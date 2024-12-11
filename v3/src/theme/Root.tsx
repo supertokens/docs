f41469e10764f728c8ac10abd4afc4730ba16b33
Bogdan Carpusor <bogdan@supertokens.com>
2024-12-11 16:05:16 +0200

    Fix styles

--- v3/src/theme/Root.tsx
+++ v3/src/theme/Root.tsx
@@ -2,17 +2,15 @@ import { Theme } from "@radix-ui/themes";
 
 import { DocItemContextProvider } from "../components/DocItemContext";
 
-import "@radix-ui/themes/styles.css";
-
 export default function Root({ children }: { children: React.ReactNode }) {
-  return (
-    <Theme
-      accentColor="orange"
-      grayColor="gray"
-      appearance="dark"
-      className="theme-root"
-    >
-      <DocItemContextProvider>{children}</DocItemContextProvider>
-    </Theme>
-  );
+	return (
+		<Theme
+			accentColor="orange"
+			grayColor="gray"
+			appearance="dark"
+			className="theme-root"
+		>
+			<DocItemContextProvider>{children}</DocItemContextProvider>
+		</Theme>
+	);
 }
