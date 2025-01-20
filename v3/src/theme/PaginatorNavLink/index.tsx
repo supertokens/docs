dee0154060dcdf607225b79b997e5e42be0705fb
Bogdan Carpusor <bogdan@supertokens.com>
2025-01-20 10:16:15 +0200

    Simplify the flow

--- v3/src/theme/PaginatorNavLink/index.tsx
+++ v3/src/theme/PaginatorNavLink/index.tsx
@@ -1,24 +1,25 @@
 import React, { type ReactNode } from "react";
-import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import type { Props } from "@theme/PaginatorNavLink";
 
-import { Card, Flex, Text } from "@radix-ui/themes";
+import { Card, Flex, Grid, Text } from "@radix-ui/themes";
 
 export default function PaginatorNavLink(props: Props): ReactNode {
   const { permalink, title, subLabel, isNext } = props;
   return (
-    <Card asChild>
-      <Link className="pagination-nav__link" to={permalink}>
-        <Flex direction="column" align={isNext ? "end" : "start"}>
-          <Text as="div" size="2" weight="bold">
-            {title}
-          </Text>
-          <Text as="div" size="2" color="gray">
-            {subLabel}
-          </Text>
-        </Flex>
-      </Link>
-    </Card>
+    <Grid gridColumn={isNext ? "2/3" : "unset"} asChild>
+      <Card asChild>
+        <Link className="pagination-nav__link" to={permalink}>
+          <Flex direction="column" align={isNext ? "end" : "start"}>
+            <Text as="div" size="2" weight="bold">
+              {title}
+            </Text>
+            <Text as="div" size="2" color="gray">
+              {subLabel}
+            </Text>
+          </Flex>
+        </Link>
+      </Card>
+    </Grid>
   );
 }
