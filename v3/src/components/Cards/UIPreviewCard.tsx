27631eff69ab9251dc83b285d5da5049d444e4aa
Bogdan Carpusor <bogdan@supertokens.com>
2025-02-22 18:29:55 +0200

    Update additional docs

--- v3/src/components/Cards/UIPreviewCard.tsx
+++
@@ -1,9 +0,0 @@
-import { Flex, Box, Card, Separator } from "@radix-ui/themes";
-
-export function UIPreviewCard({ children }: React.PropsWithChildren<{}>) {
-  return (
-    <Box p="0" asChild>
-      <Card>{children}</Card>
-    </Box>
-  );
-}
