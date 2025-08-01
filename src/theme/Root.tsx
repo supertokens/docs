c8433fec7aedd2b028477bca63481208730edb1a
Bogdan Carpusor <bogdan@supertokens.com>
2025-08-01 19:37:28 +0300

    Fix the heading scroll position

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -18,8 +18,6 @@ export default function Root({ children }: { children: React.ReactNode }) {
     init();
   }, []);
 
-  useLayoutEffect(() => {}, []);
-
   useEffect(() => {
     trackPageView();
   }, [pathname]);
